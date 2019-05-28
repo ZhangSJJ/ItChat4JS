ca3e456839f0ce23b8adb27147dede9e54aa4f16
zhangsjj <2012zhangsj@sina.cn>
2019-05-28 15:26:31 +0800

    ‘doingdoing web init

--- src/Fetch.js
+++ src/Fetch.js
@@ -3,15 +3,16 @@ import querystring from 'querystring'
 import { USER_AGENT } from "./Config";
 
 const Fetch = (url, options = {}) => {
-    const { headers, method = 'GET', json, timeout, ...restOptions } = options;
+    const { headers, method = 'GET', json, timeout, redirect = 'follow', ...restOptions } = options;
     const methodUpCase = method.toUpperCase();
 
     let finalOptions = {
+        redirect,
         method: methodUpCase,
         credentials: 'include',
         headers: {
             'User-Agent': USER_AGENT,
-            'Content-Type': 'application/json',
+            'Content-Type': 'application/json;charset=UTF-8',
             ...headers
         },
 
@@ -65,3 +66,43 @@ export function toJSON(response) {
 
 
 export default Fetch;
+
+export const  Fetch111 = (url, options = {}) => {
+    const { method, ...restOptions } = options;
+
+    let finalOptions = {
+        method: method || 'POST',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+            ...restOptions.headers
+        },
+
+    };
+    if (Object.keys(restOptions).length) {
+        finalOptions.body = JSON.stringify(restOptions)
+    }
+    /**
+     * 浏览器端的 fetch 有 credentials: 'include'，会自动带上 cookie
+     * 服务端得手动设置，可以从 context 对象里取 cookie
+     */
+
+    let fetchData = fetch(url, finalOptions)
+
+    /**
+     * 拓展字段，如果手动设置 options.json 为 false
+     * 不自动 JSON.parse
+     */
+    if (restOptions.json !== false) {
+        fetchData = fetchData.then(toJSON)
+    }
+
+    /**
+     * 设置自动化的超时处理
+     */
+    if (typeof restOptions.timeout === 'number') {
+        fetchData = timeoutReject(fetchData, restOptions.timeout)
+    }
+
+    return fetchData
+};
--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
  * @time 2019/5/27
  */
 
+import parser from 'fast-xml-parser';
 import { BASE_URL, APP_ID } from './Config';
-import  Fetch  from './Fetch';
+import Fetch, { Fetch111 } from './Fetch';
 
 import { whileDoing } from './Utils';
 
@@ -51,12 +52,22 @@ const processLoginInfo = async (resText) => {
     const match = resText.match(reg);
     const redirectUrl = match[1];
     self.redirectUrl = redirectUrl;
-    const res = await Fetch(redirectUrl, { json: false });
+    self.loginUrl = redirectUrl.slice(0, redirectUrl.lastIndexOf('/'));
+    const res = await Fetch(redirectUrl, { json: false, redirect: 'manual' });
+    const buffer = new Buffer(res.body._buffer).toString();
+    const { ret, skey, wxsid, wxuin, pass_ticket } = ((parser.parse(buffer)) || {}).error || {};
+    if (ret === 0 && !!skey && !!wxsid && !!wxuin && !!pass_ticket) {
+        self.BaseRequest = {};
+        self.skey = self.BaseRequest.skey = skey;
+        self.wxsid = self.BaseRequest.wxsid = wxsid;
+        self.wxuin = self.BaseRequest.wxuin = wxuin;
+        self.pass_ticket = pass_ticket;
+        self.BaseRequest.DeviceID = 'e' + ((Math.random() + '').substring(2, 17));
+        await webInit();
+    } else {
+        console.log(`Your wechat account may be LIMITED to log in WEB wechat, error info:${buffer}`)
+    }
 
-    const bufferText = convertRes(res);
-    const buffer = new Buffer(res.body._buffer);
-
-    console.log(buffer.toString())
 };
 
 const checkUserLogin = async (userId) => {
@@ -94,7 +105,17 @@ const checkUserLogin = async (userId) => {
     return res;
 };
 
-const webInit = () => {
+const webInit = async () => {
+    const now = Date.now();
+    const url = `${self.loginUrl}?r=${Math.floor(-now / 1579)}&lang=zh_CN&pass_ticket=${self.pass_ticket}`;
+    console.log(url)
+    const params = {
+        BaseRequest: self.BaseRequest,
+        method: 'post',
+        json: false
+    };
+    const res = await Fetch(url, params);
+    console.log(res)
 
 };
 
@@ -105,18 +126,36 @@ const fn = async () => {
 
     self.intervalId = whileDoing(async () => {
         await checkUserLogin(userId);
-        webInit();
     })
 
 
 };
 
-fn()
+// fn()
 
+// var data = '<error><ret>0</ret><message></message><skey>@crypt_2cea0d31_3477374853f8e7005293353ccdb1006e</skey><wxsid>py6LmvsRhrrFzLeu</wxsid><wxuin>452613396</wxuin><pass_ticket>9+6tj4AepAbuT+h34vUlmIlzT+lF1dyaulpsZi721HLlK+MUhj+n4ExmLQV3bW9Q</pass_ticket><isgrayscale>1</isgrayscale></error>';
+//
+// console.log(parser.parse(data))
 
-
-
-
+Fetch111('https://gateway.m.uat.qa.nt.ctripcorp.com/restapi/soa2/16357/json/voiceDetail', {
+    "ver": "8.3.2.1",
+    "pageid": 10650015942,
+    "resourceid": 1000624081,
+    "head": {
+        "cid": "09031149111446967275",
+        "ctok": "",
+        "cver": "1.0",
+        "lang": "01",
+        "sid": "8888",
+        "syscode": "09",
+        "auth": null,
+        "extension": [{ "name": "networkstatus", "value": "None" }, { "name": "protocal", "value": "http" }]
+    },
+    "contentType": "json",
+    method:'post'
+}).then(res=>{
+    console.log(res)
+})
 
 
 
